d050be0df47294c0bee2b9b8c952e5178e1fc792
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-06-20 12:38:23 +0530

    sorted new approch

--- 1_Day_Array/10_Merge_Sorted.js
+++ 1_Day_Array/10_Merge_Sorted.js
@@ -13,22 +13,69 @@
 // The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 
 
-function mergesort(nums1,nums2){
-    let newarr=[]
-    for(let i=0;i<nums1.length;i++){
-        for(let j=0;j<nums2.length;j++){
-            if(nums1[i]>nums2[j]){
-                newarr.push(nums2[j])
-            }
-            if(nums1[i]<nums2[j]){
-                newarr.push(nums1[i])
-            }
+// function mergesort(nums1,nums2){
+//     let newarr=[]
+//     for(let i=0;i<nums1.length;i++){
+//         for(let j=0;j<nums2.length;j++){
+//             if(nums1[i]>nums2[j]){
+//                 newarr.push(nums2[j])
+//                 j++
+//             }
+//             if(nums1[i]<nums2[j]){
+//                 newarr.push(nums1[i])
+//                 i++
+//             }
 
+//         }
+//     }return newarr.sort()
+// }
+// const result= mergesort([1,2,3,0,0,0],[2,5,6])
+// console.log(result)
+
+
+
+function merge(nums1,m,nums2,n){
+    let nums1copy=[]
+    for(let i=0;i<m;i++){
+        nums1copy[i]=nums1[i]
+    }
+    let p1=0
+    let p2=0
+
+    for(let j=0;j<m+n;j++){
+        if(p2>=n || (p1<m && nums1copy[p1]<nums2[p2])){
+            nums1[j]=nums1copy[p1]
+            p1++
+        }else{
+            nums1[j]=nums2[p2]
+            p2++
         }
-    }return newarr.sort()
+    }
+
 }
-const result= mergesort([1,2,3,0,0,0],[2,5,6])
+
+const result= merge([1,2,3,0,0,0],3,[2,5,6],3)
 console.log(result)
 
 
 
+// this aproch when there is no 0 in array
+
+function mergesor(arr1,arr2){
+    let result=[]
+    while(arr1.length && arr1.length){
+        let next= (arr1[0]<arr2[0]?arr1.shift():arr2.shift())
+        result.push(next)
+    }
+    if(arr1.length){
+        result= result.concat(arr1)
+    }
+    else if(arr2.length){
+        result= result.concat(arr2)
+    }
+    return result
+
+}
+
+const res=mergesor([1,2,3,0,0,0],[2,5,6])
+console.log(res)
