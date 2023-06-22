32c8ce08684f740825fb11722c7bea3b93430807
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-06-22 16:27:33 +0530

    duplicte updated

--- 2_Day/2_Duplicate_Num.js
+++ 2_Day/2_Duplicate_Num.js
@@ -6,8 +6,6 @@
 
 // You must solve the problem without modifying the array nums and uses only constant extra space.
 
- 
-
 // Example 1:
 
 // Input: nums = [1,3,4,2,2]
@@ -16,3 +14,43 @@
 
 // Input: nums = [3,1,3,4,2]
 // Output: 3
+
+// sorting
+
+function dp(nums) {
+  nums.sort();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return nums[i];
+    }
+  }
+  return -1;
+}
+const res1 = dp([1, 3, 4, 2, 2]);
+//console.log(res1);
+
+// hashmap
+
+// function dup(arr) {
+//   let map = {};
+//   for (let i of arr) {
+//     if (!map[i]) {
+//       map[i] = 1;
+//     } else return i;
+//   }
+// }
+
+function dup(nums) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else return nums[i];
+  }
+}
+
+const result = dup([3, 1, 3, 4, 2]);
+
+console.log(result);
+
+// we can use Floyd's Algorithm
