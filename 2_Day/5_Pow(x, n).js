ea5f18d2268a99816c24638fe3371bfe436e5a35
Avinabh Kumar <kumar.avi3112@gmail.com>
2023-06-22 17:04:09 +0530

    solved power_n

--- 2_Day/5_Pow(x, n).js
+++ 2_Day/5_Pow(x, n).js
@@ -1,8 +1,19 @@
 // Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
- 
-
 // Example 1:
 
 // Input: x = 2.00000, n = 10
 // Output: 1024.00000
+
+function power(x, n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return 1 / power(x, -n);
+  }
+  return x * power(x, n - 1);
+}
+
+const res = power(2, 10);
+console.log(res);
